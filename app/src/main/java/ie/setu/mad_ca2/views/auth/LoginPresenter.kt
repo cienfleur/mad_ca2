60e9fb4fdc9272b1a0cf2e8d4bafebb6fb88a749
cienfleur <redvelvet0214@protonmail.ch>
2025-12-13 15:22:34 +0000

    Finish authentication page, successfully integrated with firebase

--- app/src/main/java/ie/setu/mad_ca2/views/auth/LoginPresenter.kt
+++ app/src/main/java/ie/setu/mad_ca2/views/auth/LoginPresenter.kt
@@ -2,12 +2,21 @@ package ie.setu.mad_ca2.views.auth
 
 import android.util.Patterns
 import ie.setu.mad_ca2.main.MainApp
+import android.app.Activity
+import android.content.Intent
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import ie.setu.mad_ca2.views.companylist.CompanyListView
 
 class LoginPresenter(val view: LoginView) {
 
     var app: MainApp = view.application as MainApp
 
     private var isLogin = true
+
+    private lateinit var loginIntentLauncher: ActivityResultLauncher<Intent>
+
+
 
     init {
         view.showLoginState()
@@ -52,12 +61,29 @@ class LoginPresenter(val view: LoginView) {
 
 
     private fun doLogin(email: String, pass: String) {
-        // TODO: Add Firebase Auth logic here later
         view.showSnackBar("Logging in $email...")
+        app.auth.signInWithEmailAndPassword(email, pass).addOnCompleteListener(view) { task ->
+            if (task.isSuccessful) {
+                view.showSnackBar("Login Successful")
+                // Navigate to the main activity and load data
+                val launcherIntent = Intent(view, CompanyListView::class.java)
+                view.startActivity(launcherIntent)
+            } else {
+                view.showSnackBar("Login Failed: ${task.exception?.message}")
+            }
+        }
     }
 
     private fun doRegister(email: String, pass: String) {
-        // TODO: Add Firebase Auth logic here later
         view.showSnackBar("Registering $email...")
+        app.auth.createUserWithEmailAndPassword(email, pass).addOnCompleteListener(view) { task ->
+            if (task.isSuccessful) {
+                view.showSnackBar("Registration Successful")
+                // return to login state
+                doToggleAuthMode()
+            } else {
+                view.showSnackBar("Registration Failed: ${task.exception?.message}")
+            }
+        }
     }
 }
